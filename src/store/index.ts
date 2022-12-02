963b6626ecb802f45e0f137d666edee85b494c8e
Deagen01 <a602593639@163.com>
2022-12-02 16:09:11 +0800

    merge

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 
 import { defineStore } from "pinia";
-import { colorNode } from "@/lib/node/simpleNode";
+import { colorNode } from "@/lib/node/generatorNode";
 import { Node } from "@/lib/node/Node";
 import { Property } from "@/lib/node/NodeProperty";
 export const useMainStore = defineStore('main', {
