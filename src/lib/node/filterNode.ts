5318a49009315f8d954b56c0a63bdb551dd5ff6b
Deagen01 <a602593639@163.com>
2023-02-22 11:24:06 +0800

    feat(cmf):blend节点添加enum属性

--- src/lib/node/filterNode.ts
+++ src/lib/node/filterNode.ts
@@ -78,19 +78,71 @@ export class BlendNode extends Node{
         this.addInput("Background");
         // this.addInput("Opacity");
         this.addFloatProperty("Opacity","Opacity",0.7, 0.0, 1.0, 0.01);
-
+		this.addEnumProperty("Type", "Type", [
+			"Multiply",
+			"Add",
+			"Subtract",
+			"Divide",
+			//   "Add Sub",
+			"Max",
+			"Min",
+			"Switch",
+			"Overlay",
+			"Screen"
+		]);
         this.fragmentSource = `precision mediump float;\n`+
         this.createCodeForProps()+
         this.createCodeForInputs()+
         `
         varying vec2 vTexCoord;
 
+        float screen(float fg, float bg) {
+            float res = (1.0 - fg) * (1.0 - bg);
+            return 1.0 - res;
+        }
+        
         void main(){
             vec4 foreground = texture2D(inputForeground,vTexCoord);
             vec4 background = texture2D(inputBackground,vTexCoord);
-            vec4 result = mix(background,foreground,propOpacity);
-           
-            gl_FragColor =result;
+            // vec4 result = mix(background,foreground,propOpacity);
+            vec4 colA = foreground;
+            vec4 colB = background;
+            vec4 col = vec4(1.0);
+
+            if (propType==0){ // multiply
+                col.rgb = colA.rgb * colB.rgb;
+            }
+            if (propType==1) // add
+                col.rgb = colA.rgb + colB.rgb;
+            if (propType==2) // subtract
+                col.rgb = colB.rgb - colA.rgb;
+            if (propType==3) // divide
+                col.rgb = colB.rgb / colA.rgb;
+            if (propType==4) { // max
+                col.rgb = max(colA.rgb, colB.rgb);
+            }
+            if (propType==5) { // min
+                col.rgb = min(colA.rgb, colB.rgb);
+            }
+            if (propType==6) { // switch
+                col.rgb = colA.rgb;
+            }
+            if (propType==7) { // overlay
+                if (colB.r < 0.5) col.r = colB.r * colA.r; else col.r = screen(colB.r, colA.r);
+                if (colB.g < 0.5) col.g = colB.g * colA.g; else col.g = screen(colB.g, colA.g);
+                if (colB.b < 0.5) col.b = colB.b * colA.b; else col.b = screen(colB.b, colA.b);
+            }
+            if (propType==8) { // screen
+                col.r = screen(colA.r, colB.r);
+                col.g = screen(colA.g, colB.g);
+                col.b = screen(colA.b, colB.b);
+            }
+
+            // apply opacity 
+            //后期继续加上Opacity的输入纹理 比如噪声图
+            col.rgb = mix(colB.rgb, col.rgb, vec3(propOpacity));
+
+            gl_FragColor =col;
         }        
         `
         ;
