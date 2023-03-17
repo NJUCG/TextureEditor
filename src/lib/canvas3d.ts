0a4b07092c45f532473a638d2c73580c9a184b9c
sq_win <745356675@qq.com>
2023-03-17 23:25:23 +0800

    feat(sq): 整理canvas3d，新增threejs的pbrUtilities

--- src/lib/canvas3d.ts
+++ src/lib/canvas3d.ts
@@ -7,6 +7,7 @@ import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { SphereGeometry } from "./geometry/sphere";
 import { DataTexture } from "three";
+import { TextureCanvas } from "./utils/texture-canvas";
 
 // OrbitControls in typescript: https://stackoverflow.com/questions/19444592/using-threejs-orbitcontols-in-typescript/56338877#56338877
 // OrbitControls docs: https://threejs.org/docs/#examples/zh/controls/OrbitControls
@@ -16,81 +17,161 @@ import { DataTexture } from "three";
  * https://blog.csdn.net/weixin_43854673/article/details/119671467
  */
 
+export const mappingChannelName: string[] = ["BaseColor", "Roughness", "Metallic", "AmbientOcclusion", "Normal", "Height"];
+
+export enum MappingChannel {
+    BaseColor,
+    Roughness,
+    Metallic,
+    AmbientOcclusion,
+    Normal,
+    Height,
+}
+
+enum PBRWorkflow {
+    Metallic,
+    Specular,
+    // other pbr workflow
+};
+
 export class View3D {
-    private scene!: THREE.Scene;
-    private camera!: THREE.PerspectiveCamera;
-    private renderer!: THREE.WebGLRenderer;
-    private controls: OrbitControls | null = null;
-    private model: THREE.Object3D | null = null;
-    private material: THREE.MeshStandardMaterial = new THREE.MeshStandardMaterial(
-        {
-            // color: 0x3F51B5
-            color: 0xffffff,
-			roughness: 0.5,
-			metalness: 0.0,
-			transparent: true,
-			// alphaTest: 0,
-			// depthFunc: THREE.AlwaysDepth,
-			side: THREE.FrontSide,
-			blending: THREE.NormalBlending
-        }
-    );
+    private scene: THREE.Scene;
+    private camera: THREE.PerspectiveCamera;
+    private renderer: THREE.WebGLRenderer;
+    private controls: OrbitControls;
+    private model: THREE.Object3D;
+    private material: THREE.MeshStandardMaterial;
+    private pbrWorkflow: PBRWorkflow;
 
     // private cubuMap: THREE.CubeTexture | null = null;
-    private skyPath = "./assets/env/wide_street_01_1k.hdr";
-    private envMap: THREE.Texture | null = null;
-    private texRepeat = 1;
+    private skyPath: string;
+    private envMap: THREE.Texture;
+    private texRepeat: number;
 
     // geometry
-	private sphereGeom = new SphereGeometry(0.7, 128, 128);
-	private cubeGeom = new THREE.BoxGeometry();
+	private sphereGeom: SphereGeometry;
+	private cubeGeom: THREE.BoxGeometry;
 	// private planeGeom = new PlaneGeometry(2, 2, 100, 100);
 	// private cylinderGeom = new CylinderGeometry(0.5, 0.5, 1, 64, 64, true);
 
-    setSkyPath(path: string) {
+    constructor() {
+        this.pbrWorkflow = PBRWorkflow.Metallic;
+
+        this.skyPath = "./assets/env/wide_street_01_1k.hdr";
+        this.envMap = null;
+        this.texRepeat = 1;
+        
+        this.sphereGeom = new SphereGeometry(0.7, 128, 128);
+        this.cubeGeom = new THREE.BoxGeometry();
+    }
+
+    public setSkyPath(path: string) {
         this.skyPath = path;
         this.loadEnvEquirect();
     }
 
-    setCanvas(canvas: HTMLCanvasElement) {
+    public setCanvas(canvas: HTMLCanvasElement) {
         this.setupRenderer(canvas);
         this.setupScene();
+        this.setupModelAndMaterial();
         this.setupCamera(45, canvas.width / canvas.height, 0.1, 1000, 1, 1, 2.6);
+        this.setupOrbitControls(canvas);
         this.setupLighting();
         this.loadEnvEquirect();
-        this.setupOrbitControls(canvas);
-        // this.setupBackground();
-
-        // const modelDirPath = "./assets/models";
-        // const modelFileFormat = "obj";
-        // const modelName = "paimon";
-        // this.loadModel(modelDirPath, modelFileFormat, modelName);
-        this.model = new THREE.Mesh(this.sphereGeom, this.material);
-        
-        this.scene.add(this.model);
-
 
         const animate = (() => {
             requestAnimationFrame(animate);
-            this.controls?.update();
+            this.controls.update();
             this.renderer.render(this.scene, this.camera);
         });
 
         animate();
     }
 
-    setupScene() {
-        this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color('#eee');
-        this.scene.add(new THREE.AxesHelper(20));
-        // this.scene.fog = new THREE.Fog("#eee", 20, 100);
+    public resize(width: number, height: number) {
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(width, height);
+    }
+
+    /* Metallic-Roughness Workflow */
+    public setBaseColorTexture(texCanvas: TextureCanvas) {
+        const tex = new THREE.CanvasTexture(texCanvas.canvas);
+		tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+		tex.repeat.set(this.texRepeat, this.texRepeat);
+		tex.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+
+		tex.needsUpdate = true;
+		this.material.map = tex;
+		this.material.needsUpdate = true;
+    }
+
+    public setRoughnessTexture(texCanvas: TextureCanvas) {
+        const tex = new THREE.CanvasTexture(texCanvas.canvas);
+		tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+		tex.repeat.set(this.texRepeat, this.texRepeat);
+		tex.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+
+		tex.needsUpdate = true;
+		this.material.roughnessMap = tex;
+        this.material.roughness = 1.0;
+		this.material.needsUpdate = true;
+    }
+
+    public setMetallicTexture(texCanvas: TextureCanvas) {
+		const tex = new THREE.CanvasTexture(texCanvas.canvas);
+		tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+		tex.repeat.set(this.texRepeat, this.texRepeat);
+		tex.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+
+		tex.needsUpdate = true;
+		this.material.metalnessMap = tex;
+		this.material.metalness = 1.0;
+		this.material.needsUpdate = true;
+	}
+
+    /* Specular-Glossiness Workflow */
+
+    public setAmbientOcclusionTexture(texCanvas: TextureCanvas) {
+        const tex = new THREE.CanvasTexture(texCanvas.canvas);
+		tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+		tex.repeat.set(this.texRepeat, this.texRepeat);
+		tex.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+
+		tex.needsUpdate = true;
+		this.material.aoMap = tex;
+        this.material.aoMapIntensity = 1.0;
+		this.material.needsUpdate = true;
+    }
+
+    public setNormalTexture(texCanvas: TextureCanvas) {
+        const tex = new THREE.CanvasTexture(texCanvas.canvas);
+		tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+		tex.repeat.set(this.texRepeat, this.texRepeat);
+		tex.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+
+		tex.needsUpdate = true;
+		this.material.normalMap = tex;
+		this.material.needsUpdate = true;
+    }
+
+    public setHeightTexture(texCanvas: TextureCanvas) {
+        const tex = new THREE.CanvasTexture(texCanvas.canvas);
+		tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+		tex.repeat.set(this.texRepeat, this.texRepeat);
+		tex.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+
+		tex.needsUpdate = true;
+		this.material.displacementMap = tex;
+        this.material.displacementScale = 1.0;
+		this.material.needsUpdate = true;
     }
 
     /* WebGLRenderer docs
-     * https://threejs.org/docs/#api/zh/renderers/WebGLRenderer
-     * https://threejs.org/docs/#api/zh/constants/Renderer
-     */
-    setupRenderer(canvas: HTMLCanvasElement) {
+    * https://threejs.org/docs/#api/zh/renderers/WebGLRenderer
+    * https://threejs.org/docs/#api/zh/constants/Renderer
+    */
+    private setupRenderer(canvas: HTMLCanvasElement) {
         const renderer = new THREE.WebGLRenderer({
             alpha: true,
             canvas: canvas, 
@@ -107,7 +188,33 @@ export class View3D {
         this.renderer = renderer;
     }
 
-    setupCamera(fov: number, aspect: number, near: number, far: number, x: number, y: number, z: number) {
+    private setupScene() {
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color('#eee');
+        this.scene.add(new THREE.AxesHelper(20));
+        // this.scene.fog = new THREE.Fog("#eee", 20, 100);
+    }
+
+    private setupModelAndMaterial() {
+        this.material = new THREE.MeshStandardMaterial(
+            {
+                // color: 0x3F51B5
+                color: 0xffffff,
+                roughness: 0.5,
+                metalness: 0.0,
+                transparent: true,
+                // alphaTest: 0,
+                // depthFunc: THREE.AlwaysDepth,
+                side: THREE.FrontSide,
+                blending: THREE.NormalBlending
+            }
+        );
+
+        this.model = new THREE.Mesh(this.sphereGeom, this.material);
+        this.scene.add(this.model);
+    }
+
+    private setupCamera(fov: number, aspect: number, near: number, far: number, x: number, y: number, z: number) {
         this.camera = new THREE.PerspectiveCamera(
             fov,
             aspect,
@@ -119,7 +226,7 @@ export class View3D {
         this.camera.position.x = x;
     }
 
-    setupOrbitControls(canvas: HTMLCanvasElement) {
+    private setupOrbitControls(canvas: HTMLCanvasElement) {
         const controls = new OrbitControls(this.camera, canvas);
         // settings of orbitcontrols
         controls.enableZoom = true;
@@ -130,53 +237,60 @@ export class View3D {
         this.controls = controls;
     }
 
-    // 提供一个地面, 测试光照阴影
-    setupBackground() {
-        const floorGeometry = new THREE.PlaneGeometry(300, 300);
-        const floorMaterial = new THREE.MeshPhongMaterial({ color: 0x857ebb, shininess: 0 });
-        const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-        floor.rotation.x = -0.5 * Math.PI;
-        floor.receiveShadow = true;
-        floor.position.y = -0.001;
-        this.scene.add(floor);
-    }
-
-    setupLighting() {
+    private setupLighting() {
         const container = new THREE.Object3D();
 
         const brightness = 2;
 
         let object3d = new THREE.DirectionalLight("white", 0.225 * brightness);
-		object3d.position.set(2.6, 1, 3);
-		object3d.name = "Back light";
-		container.add(object3d);
+        object3d.position.set(2.6, 1, 3);
+        object3d.name = "Back light";
+        container.add(object3d);
 
-		object3d = new THREE.DirectionalLight("white", 0.375 * brightness);
-		object3d.position.set(-2, -1, 0);
-		object3d.name = "Key light";
-		container.add(object3d);
+        object3d = new THREE.DirectionalLight("white", 0.375 * brightness);
+        object3d.position.set(-2, -1, 0);
+        object3d.name = "Key light";
+        container.add(object3d);
 
-		object3d = new THREE.DirectionalLight("white", 0.75 * brightness);
-		object3d.position.set(3, 3, 2);
-		object3d.name = "Fill light";
-		container.add(object3d);
+        object3d = new THREE.DirectionalLight("white", 0.75 * brightness);
+        object3d.position.set(3, 3, 2);
+        object3d.name = "Fill light";
+        container.add(object3d);
 
         this.scene.add(container);
-        // this.scene.add(directLight);
-        // this.addCameraHelper(directLight.shadow.camera);
     }
 
-    resize(width: number, height: number) {
-        this.camera.aspect = width / height;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(width, height);
+    private loadEnvEquirect() {
+        const skyPath = this.skyPath;
+        const hdrPath = skyPath;
+
+        new RGBELoader()
+            .setDataType(THREE.FloatType)
+            .load(hdrPath, (texure: DataTexture) => {
+                if (skyPath !== this.skyPath)
+                    return;
+                const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+                pmremGenerator.compileEquirectangularShader();
+
+                const evtRT = pmremGenerator.fromEquirectangular(texure);
+                const tex = evtRT.texture;
+                tex.minFilter = THREE.LinearFilter;
+                tex.magFilter = THREE.LinearFilter;
+
+                this.material.envMap = tex;
+                this.material.needsUpdate = true;
+                this.scene.background = tex;
+                // 在此次更新材质的环境贴图
+                this.envMap = new THREE.Texture();
+                this.envMap.copy(tex);
+            });
     }
 
-    addCameraHelper(camera: THREE.Camera) {
+    private addCameraHelper(camera: THREE.Camera) {
         this.scene.add(new THREE.CameraHelper(camera));
     }
 
-    setModel(modelName: string) {
+    private setModel(modelName: string) {
 		if (this.model) this.scene.remove(this.model);
 
 		let geometry: THREE.BufferGeometry;
@@ -190,7 +304,7 @@ export class View3D {
 		this.scene.add(this.model);
 	}
 
-    loadModel(modelDirPath: string, modelFileFormat: string, modelName: string) {
+    private loadModel(modelDirPath: string, modelFileFormat: string, modelName: string) {
         if (this.model)
             this.scene.remove(this.model);
 
@@ -263,31 +377,5 @@ export class View3D {
                 console.error("Unexpected model file format: " + modelFileFormat + "!");
                 return;
         }
-    }
-
-    loadEnvEquirect() {
-        const skyPath = this.skyPath;
-        const hdrPath = skyPath;
-
-        new RGBELoader()
-            .setDataType(THREE.FloatType)
-            .load(hdrPath, (texure: DataTexture) => {
-                if (skyPath !== this.skyPath)
-                    return;
-                const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
-                pmremGenerator.compileEquirectangularShader();
-
-                const evtRT = pmremGenerator.fromEquirectangular(texure);
-                const tex = evtRT.texture;
-                tex.minFilter = THREE.LinearFilter;
-                tex.magFilter = THREE.LinearFilter;
-
-                this.material.envMap = tex;
-                this.material.needsUpdate = true;
-                this.scene.background = tex;
-                // 在此次更新材质的环境贴图
-                this.envMap = new THREE.Texture();
-                this.envMap.copy(tex);
-            });
     }
 }
