1939f90cccd4377f240f7aa047f0ab2e2ca46f2e
sq_win <745356675@qq.com>
2023-04-06 01:18:22 +0800

    fix(sq): 解决新建、打开项目的刷新问题

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
         <template #A>
             <split-view direction="vertical">
                 <template #A>
-                    <View2D v-if="hasProject"></View2D>
+                    <View2D v-if="isVisible" :key="refreshTick"></View2D>
                 </template>
                 <template #B>
-                    <View3D v-if="hasProject"></View3D>
+                    <View3D v-if="isVisible" :key="refreshTick"></View3D>
                 </template>
             </split-view>
         </template>
@@ -14,13 +14,13 @@
         <template #B>
             <split-view direction="horizontal" a-init="75%">
                 <template #A>
-                    <EditorView v-if="hasProject" ref="editorView" :isNewProject="isNewProject"></EditorView>
+                    <EditorView v-if="isVisible" :key="refreshTick" ref="editorView" :isNewProject="isNewProject"></EditorView>
                 </template>
                 <template #B>
                     <split-view direction="vertical">
                         <template #A>
                             PropertyView
-                            <PropertyView v-if="hasProject"></PropertyView>
+                            <PropertyView v-if="isVisible" :key="refreshTick"></PropertyView>
                         </template>
                         <template #B>
                             <LibraryView ref="libraryView"></LibraryView>
@@ -52,30 +52,26 @@ import { ipcRenderer } from "electron";
 
 const editorView = ref(null);
 
-const hasProject = ref(false);
+const refreshTick = ref(0);
+const isVisible = ref(false);
 const isNewProject = ref(false);
 
 // 处理menu指令
 ipcRenderer.on(MenuCommands.FileOpen, () => {
-    hasProject.value = false;
     isNewProject.value = false;
-    nextTick(() => {
-        const project = ProjectManager.open();
-        Editor.load(project.data);
-        hasProject.value = true;
-        ProjectManager.setWindowTitle(project.name);
-    });
+    const project = ProjectManager.open();
+    Editor.load(project.data);
+    ProjectManager.setWindowTitle(project.name);
+    isVisible.value = true;
+    refreshTick.value = (refreshTick.value + 1) % 100;
 })
 
 ipcRenderer.on(MenuCommands.FileNew, () => {
-    hasProject.value = false;
-    isNewProject.value = false;
-    nextTick(() => {
-        const project = ProjectManager.new();
-        hasProject.value = true;
-        isNewProject.value = true;
-        ProjectManager.setWindowTitle(project.name);
-    });
+    isNewProject.value = true;
+    const project = ProjectManager.new();
+    ProjectManager.setWindowTitle(project.name);
+    isVisible.value = true;
+    refreshTick.value = (refreshTick.value + 1) % 100;
 })
 
 </script>
